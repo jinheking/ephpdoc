d473a2d9de024d5cd0e5b90d0c4f95f14babbbcb
jinheking <jinheking@sina.com>
2012-12-26 14:18:11 +0800

    restart

--- try.py
+++ try.py
@@ -4,12 +4,13 @@ import re
 
 
 if '__main__' == __name__:
-  s=r'<ul id="menu"><li>1</li></ul>'
-  reg=r'<ul id="menu">(?P<body>.*?)</ul>'
-  mi= re.compile(reg)
-  m=mi.finditer(s)
-  for my in m:
-    content=r'<ul id="menu">'+my.group('body')+r'<li>美丽人生</li>'+r'</ul>'
-    result, number = re.subn(reg, content, s) 
-    print unicode(result,"utf-8").encode("GBK")
+    s=u'<ul id="menu"><li>1</li></ul>'
+    reg=u'<ul id="menu">(?P<body>.*?)</ul>'
+    mi= re.compile(reg)
+    m=mi.finditer(s)
+    for my in m:
+        content=u'<ul id="menu">'+my.group('body')+r'<li>美丽人生</li>'+r'</ul>'
+        result, number = re.subn(reg, content, s) 
+#    print unicode(result,"utf-8").encode("utf-8")
+        print result
 
